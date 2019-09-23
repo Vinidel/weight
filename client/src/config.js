02a724d1c238fd27b687e4f3c3f023f29595af0f
Vinicius Delascio Fortunato <vinidroid@gmail.com>
2019-09-23 17:16:44 +1000

    master Fix url

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-// export const BASE_API_URL = 'https://guarded-woodland-18511.herokuapp.com'
-export const BASE_API_URL = 'http://localhost:8081'
+export const BASE_API_URL = 'https://guarded-woodland-18511.herokuapp.com'
+// export const BASE_API_URL = 'http://localhost:8081'
