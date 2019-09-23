6cbf6006226bd65f0acee824d53da4a373487c38
Vinicius Delascio Fortunato <vinidroid@gmail.com>
2019-09-23 16:55:55 +1000

    master add date picker to component

--- client/src/components/WeightComponent.js
+++ client/src/components/WeightComponent.js
@@ -10,7 +10,7 @@ class WeightComponent extends Component {
         this.state = {
             sent: false,
             weight: 0.00,
-            date: '',
+            createdAt: '',
             history: []
         }
     }
@@ -35,13 +35,13 @@ class WeightComponent extends Component {
       this.setState({weight: event.target.value});
     }
 
-    // handleDateChange = (event) => {
-    //   this.setState({date: event.target.value});
-    // }
+    handleDateChange = (event) => {
+      this.setState({createdAt: event.target.value});
+    }
 
     handleSubmit = (e) => {
       e.preventDefault();
-      return axios.post(`${BASE_API_URL}/api/weights`, {kilograms: Number.parseFloat(this.state.weight)})
+      return axios.post(`${BASE_API_URL}/api/weights`, {kilograms: Number.parseFloat(this.state.weight), createdAt: new Date(this.state.createdAt).toISOString()})
         .then((res) => console.log('Success'))
         .then(this.getWeights)
         .catch(e => console.log('Error', e))
@@ -57,12 +57,12 @@ class WeightComponent extends Component {
                 <input type="number" className="form-control" onChange={this.handleWeightChange}  name="weight" />
               </label>
             </div>
-            {/* <div> 
+            <div> 
               <label>
                 Kg:
                 <input type="date" className="form-control" onChange={this.handleDateChange}  name="date" />
               </label>
-            </div>   */}
+            </div>  
           </form>
           <button type="button" onClick={this.handleSubmit} className="btn">Save</button>
           <br />
