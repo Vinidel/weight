996add612198950180618cd03e1707001acd9842
Vinicius Delascio Fortunato <vinidroid@gmail.com>
2019-10-25 15:10:59 +1100

    Master adding auth routes [WIP]

--- server/main.go
+++ server/main.go
@@ -10,5 +10,14 @@ func main() {
 	r := gin.Default()
 	// Dont worry about this line just yet, it will make sense in the Dockerise bit!
 	r.Use(static.Serve("/", static.LocalFile("./web", true)))
+
+	//Add a get endpoint
+	//Add a post endpoint
+	//Wrap them with an auth middleware that will redirect to google
+	r.GET("/api/weights", func(c *gin.Context){
+		c.JSON(200, gin.H{
+			"weight": "", "date": "",
+		})
+	})
 	r.Run()
 }
